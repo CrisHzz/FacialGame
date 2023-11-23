f709bc48c0025fe65cc4a5633ada4fe352637bc9
David Torres <juandis0246@gmail.com>
2023-11-22 22:30:01 -0500

    Refactor World class to add methods for adding consumables and weapons

--- src/models/World.py
+++ src/models/World.py
@@ -1,8 +1,10 @@
 import random
-from src.models.Entity import Entity, Hero, Devil, Consumable, Weapon
+from typing import Union
+from src.models.Entity import Entity, Consumable, Weapon
 
 
 class World:
+    
     def __init__(self, size: int = None) -> None:
         self.size: int = size
         self.grid: list[list] = None
@@ -30,7 +32,21 @@ class World:
         entity.y = y
         self.grid[x][y] = entity
         
-    def getEntity(self, x: int, y: int) -> Entity:
+    def addConsumable(self, consumable: Consumable, x: int = None, y: int = None) -> None:
+        if x == None or y == None:
+            x, y = self.generateRandomPosition()
+        consumable.x = x
+        consumable.y = y
+        self.grid[x][y] = consumable
+        
+    def addWeapon(self, weapon: Weapon, x: int = None, y: int = None) -> None:
+        if x == None or y == None:
+            x, y = self.generateRandomPosition()
+        weapon.x = x
+        weapon.y = y
+        self.grid[x][y] = weapon
+        
+    def getElement(self, x: int, y: int) -> Union[Entity, Consumable, Weapon, None]:
         if self._isValidPosition(x, y):
             return self.grid[x][y]
         return None
